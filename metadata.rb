6b1ece91f270d3551b41ff36b3cfb3b57bf7d2b8
Rahul Chugh <rahul.chugh@gmail.com>
2014-12-22 19:30:24 -0800

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "avishai@fewbytes.com"
 license          "Apache v2"
 description      "Installs/Configures storm"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.1.1"
 
 depends          "ark"
 depends          "java"
